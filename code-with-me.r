ca95055bd6fc99f619d437a89eed501ceadd2f53
Bovee <emily.bovee@marquette.edu>
2020-03-09 11:22:08 -0500

    all done

--- code-with-me.r
+++ code-with-me.r
@@ -4,13 +4,16 @@
 #Installing packages
 #########################################################
 
-#Here, we install 3 packages
+#Here, we install 4 packages
   #tidyverse
   #tidylog
   #readr
+  #skimr
 
-install.packages("tidyverse", "tidylog", "readr")
-
+install.packages("tidyverse")
+install.packages("tidylog") 
+install.packages("readr")
+install.packages("skimr")
 #########################################################
 #Load Packages
 #########################################################
@@ -22,6 +25,7 @@ install.packages("tidyverse", "tidylog", "readr")
 library(tidyverse)
 library(tidylog)
 library(readr)
+library(skimr)
 
 #########################################################
 #Check and Set Working Directory
@@ -29,12 +33,6 @@ library(readr)
 
 #Here, we check our working directory
 getwd()
-
-#Here, we specify the working directory that we want
-
-!!!!!!!!!!!!!
-## Finish me
-!!!!!!!!!!!!!
 
 #########################################################
 #Load Data
@@ -46,8 +44,91 @@ data <- read_csv("data.csv")
 #We can call it anything we want
 pasta <- read_csv("data.csv")
 
-#Try checking out the "data" and "pasta" objects in the environment
+#Try checking out the "data" and "pasta" objects by clicking them 
+  #You can find them at the top right, in the Environment pane
 
+#########################################################
+#Variable Renaming
+#########################################################
+
+#Let's try renaming a variable in our data
+
+#Without saving
+
+data %>% rename(testing123 = tutoring)
+  #What do you see in your console (bottom left) after running this code?
+  
+#With saving
+
+data <- data %>% 
+  rename(tutor = tutoring)
+
+#########################################################
+#Selecting Data
+#########################################################
+
+#Try filling out the rest of this code to select only the advising data and student ID data
+#Remember that variable names need to be separated by commas
+
+data_advising <- 
+  data %>% 
+  select(StudentID, , )
+
+
+#How many observations (rows) are in your new dataset?
+  
+#How many variables (columns) are in your new dataset?
+
+
+#########################################################
+#Mutating Data
+#########################################################
+#Try creating a new variable in the dataset that represents the total number of advising sessions attended
+
+#This will be the sum of the *spr_adv* and *fall_adv* variables
+
+#Save the new dataset as *awesome*
+  
+_____ <- data %>% 
+  mutate(____ = )
+
+
+#Mutating Step 2:
+colnames(awesome)
+
+#########################################################
+#Filtering Data
+#########################################################
+
+#Filter without saving
+
+data %>% filter(tutoring == 0)
+
+#Filter using your own criteria
+
+
+
+#########################################################
+#Move a variable forward
+#########################################################
+#Fill in the following code with a variable you'd like to move to the front
+
+select(data, ______, everything())
+
+
+#########################################################
+#Chaining Commands
+#########################################################
+
+data %>%
+  mutate(motivation = (int + expect)) %>% 
+  filter(tutoring == 0) %>% 
+  view
+
+#########################################################
+#Saving out our data
+#########################################################
+write_csv(data, "data_after_workshop.csv")
 
 #########################################################
 #Running linear models (regressions)
@@ -67,3 +148,4 @@ m2 <- lm(int ~ expect + tutoring + fall_adv + spr_adv,
 summary(m2)
 
 #You try: 
+#Write a linear model to predict the outcome of your choice from the dataset "data" or "pasta"
